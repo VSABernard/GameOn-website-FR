5e9641c4882c62731213146c0b4b846207eb7727
Veronica <veronica.stevania@gmail.com>
2022-04-15 23:59:14 +0200

    Check first name and last name

--- js/modal.js
+++ js/modal.js
@@ -26,3 +26,77 @@ closeModalBtn.addEventListener("click", closeModal);
 function closeModal() {
   modalbg.style.display ="none";
 }
+
+// to do list #2
+function validate() {
+  console.log('Fonction validate!');
+  const numberOfFields = formData.length;            // number of formData elements
+  console.log('numberOfFields :' + numberOfFields);
+
+  for (let i = 0; i < numberOfFields; i++) {                                  // loop to show the input's id
+    console.log('i = '+ i);
+    let inputId = formData[i].getElementsByTagName('input')[0].id;            // retrieve the id of the input
+    console.log("Input id " + inputId);
+
+    let inputValue = formData[i].getElementsByTagName('input')[0].value;      // retrieve the content value of the input
+    console.log ("inputValue" + inputValue);
+
+    // if (inputId == "birthdate") {
+    //   console.log('Happy birthday!');
+    // }
+
+    switch (inputId) {
+      case 'first':
+        checkName(inputValue);
+        break;
+      
+      case 'last':
+        checkName(inputValue);
+        break;
+
+      case 'email':
+        console.log('Check of email');
+        break;
+        
+      case 'birthdate':
+        console.log('Check of birthdate');
+        break;
+        
+      case 'quantity':
+        console.log('Check of quantity');
+        break;
+        
+      case 'location':
+        console.log('Check of location');
+        break;
+      
+      case 'checkbox':
+        console.log('Check of checkbox');
+        break;
+    }
+  }
+  return false;
+}
+
+// Check if firts name and last name has 2 characters & it's not empty
+// name : first name and last name to check
+// return true if name is correct
+function checkName (name) {
+
+  console.log ('Function check of name');
+  console.log ('Name value' + name);
+
+  if (name != "" && name.length >= 2) {
+    console.log ('Name is correct');
+    return true;
+  }
+  else {
+    console.log ('Name is incorrect');
+    return false;
+  }
+
+
+}
+
+
+
