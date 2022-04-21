73e314e4efc613455d7e896b416b44f88185832e
Veronica <veronica.stevania@gmail.com>
2022-04-21 22:44:37 +0200

    correction on the validation of general conditions' field

--- js/modal.js
+++ js/modal.js
@@ -76,7 +76,7 @@ function validate() {
     console.log ('Show error message');
   }
 
-  if (checkEmail(email) == true) {                                            // EMAIL's field
+  if (checkEmail(email) == true) {                                          // EMAIL's field
     showSucces (email);                  
   } 
   else {   
@@ -84,9 +84,9 @@ function validate() {
     console.log ('Show error message');
   }
 
-  checkBirthdate (birthdate);                                                 // BIRTHDATE's field
+  checkBirthdate (birthdate);                                               // BIRTHDATE's field
 
-  if (checkQuantity(quantity.value) == true) {                              // NUMBER OF CONTESTS' field
+  if (checkQuantity(quantity.value) == true) {                             // NUMBER OF CONTESTS' field
     showSucces (quantity);                  
   } 
   else {   
@@ -102,40 +102,34 @@ function validate() {
     console.log ('Show error message');
   }
   
-  
-
-
-
-
-
-
-
+  if (checkCheckbox(checkbox1) == true) {                                  // GENERAL CONDITIONS' field
+    showSucces (checkbox1);                  
+   } 
+   else {   
+     showError (checkbox1);                     
+     console.log ('Show error message');
+  }
+  // verification of global fields
   console.log('Fonction validate!');
-  const numberOfFields = formData.length;            // number of formData elements
+  const numberOfFields = formData.length;                                 // number of formData elements
   console.log('numberOfFields :' + numberOfFields);
 
   let numberValidFields = 0;
    
-  console.log ('numberValidFields:' + numberValidFields);
+  console.log ('numberValidFields:' + numberValidFields ++);
 
   if (numberValidFields == 7) {
     console.log ('Form is valid');
-    // return true;
+    return true;
   }
   else {
     console.log ('Form is not valid');
-    // return false;
+    return false;
   }
 }
 
 
-
-
-
-
-
-
-// CONTROL OF EACH FIELD -----------------------------------------------
+// CONTROLS OF EACH FIELD -----------------------------------------------
 
 // CONTROL if FIRST NAME and LAST NAME has 2 characters & it's not empty
 // name : first name's value or last name's value to check
@@ -254,7 +248,7 @@ function controlAge (birthdate) {
     const actualYear = today.getFullYear ();
 
     console.log ('Actual year :' + actualYear);
-    const year = birthdate.substring(0,4);                             // l'année (les quatre premiers caractères de la chaîne à partir de 0)
+    const year = birthdate.substring(0,4);                             // Year (The first 4 characters of the string starting from 0)
 
     const actualAge = actualYear - year;
     if (actualAge > 18) {
@@ -313,17 +307,19 @@ function checkLocation (radioLocation) {
 
 // ----------------------------------------------------------------------------------
 // CONTROL if the GENERAL CONDITIONS box is checked
-// parameter : conditions is a checkbox
-// return true if conditions is checked
+// parameter : checkbox1 is a checkbox
+// return true if checkbox1 is checked
 // return false if not checked or there is not checked
-function checkCheckbox (conditions) {
+function checkCheckbox () {
   
-  if (!conditions) {
-    console.log ('This is not a checkbox');
+  if (document.reserve.conditions.checked) {
+    document.getElementById("checkbox1").innerHTML = "";
+    return true;
+  }
+  else {
+    document.getElementById("checkbox1").innerHTML = "Vous devez accepter les termes et conditions";
     return false;
   }
-
-  return conditions.checked;                                        // replace if/else of the parameter
 }
 
 
