04a8036b2060998041c40f7d85eabb86a9f866c9
Veronica <veronica.stevania@gmail.com>
2022-04-28 22:53:51 +0200

    delete all the console logs

--- js/modal.js
+++ js/modal.js
@@ -8,12 +8,13 @@ function editNav() {
 }
 
 // DOM Elements
-const modal = document.querySelector('#formGlobal');
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeModalBtn = document.querySelector(".bground span.close");
+const modal = document.querySelector('#formGlobal');
 const form = document.querySelector('form');
 const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.querySelector(".bground span.close");
 const successModal = document.querySelector(".modal-thanks");
 const closeThanksBtn = document.querySelector (".btn-thanks");
 const closeFormThanks = document.getElementById('close-cross');
@@ -39,10 +40,11 @@ function closeModal() {
 
 closeModalBtn.addEventListener("click", closeModal);
 
+
 // =================================================================================================
 // =================================================================================================
 
-// TO DO LIST # 2 AND # 3 : FORM INPUTS AND VALIDATION / ADD ERROR MESSAGES
+
 
 // a global function to show a SUCCES MESSAGE and an ERROR MESSAGE 
 
@@ -66,40 +68,32 @@ document.getElementById('quantity').addEventListener('change', changeOnQuantity)
 document.getElementById('location1').addEventListener('change', changeOnLocations);
 document.getElementById('checkbox1').addEventListener('change', changeOnCheckbox1);
 
-
 function changeOnFirst (event) {
   let first = event.target;
-  console.log ('Change on first:' + first.value);
   validateFirstName (first);
-}
+}  
 function changeOnLast (event){
   let last = event.target;
-  console.log ('Change on last:' + last.value);
   validateLastName (last);
 }
 function changeOnEmail (event) {
   let email = event.target;
-  console.log ('Change on email:' + email.value);
   validateEmail (email);
 }
 function changeOnBirthdate (event) {
   let birthdate = event.target;
-  console.log ('Change on birthdate:' + birthdate.value);
   validateBirthdate (birthdate);
 }
 function changeOnQuantity (event) {
   let quantity = event.target;
-  console.log ('Change on quantity:' + quantity.value);
   validateQuantity (quantity);
 }
 function changeOnLocations (event) {
   let locations = event.target;
-  console.log ('Change on location:' + locations.value);
   validateLocations (locations);
- }
+}
 function changeOnCheckbox1 (event) {
   let checkbox1 = event.target;
-  console.log ('Change on checkbox1:' + checkbox1.value);
   validateCheckbox1 (checkbox1);
 }
 
@@ -112,8 +106,7 @@ function validateFirstName (first) {
     return true;               
   } 
   else {   
-    showError (first);                     
-    console.log ('Show error message');
+    showError (first);  
     return false;
   }
 }
@@ -123,8 +116,7 @@ function validateLastName (last) {
     return true;          
   } 
   else {   
-    showError (last);                     
-    console.log ('Show error message');
+    showError (last);   
     return false;
   }
 }
@@ -134,8 +126,7 @@ function validateEmail (email) {
     return true;
   } 
   else {   
-    showError (email);                     
-    console.log ('Show error message');
+    showError (email);  
     return false;
   }
 }
@@ -153,8 +144,7 @@ function validateQuantity (quantity) {
     return true;        
   } 
   else {   
-    showError (quantity);                     
-    console.log ('Show error message');
+    showError (quantity);   
     return false;
   }
 }
@@ -164,8 +154,7 @@ function validateLocations (locations) {
     return true;       
   } 
   else {   
-    showError (location1);                     
-    console.log ('Show error message');
+    showError (location1);     
     return false;
   }
 }
@@ -175,11 +164,11 @@ function validateCheckbox1 (checkbox1) {
     return true;       
   } 
   else {   
-    showError (checkbox1);                     
-    console.log ('Show error message');
+    showError (checkbox1);       
     return false;
   }
 }
+
 
 // add the BLUR EVENT when a field has lost focus
 // circle the field in red if it's loses focus
@@ -189,13 +178,12 @@ const formInput = document.getElementById('form');
   formInput.addEventListener('blur', onBlur, true);
 
 function onFocus(event){
-  console.log('onFocus');
-   event.target.style.border = 'thin solid red';
+  event.target.style.border = 'thin solid red';
 }
 function onBlur(event){
-  console.log('onBlur');
   event.target.style.border = '';
 }
+
 
 // check if all the fields are FILLED IN CORRECTLY
 
@@ -211,10 +199,10 @@ function validate() {
 
   let numberValidFields = 0;
 
-  // ADD ERROR MESSAGES
-  // circle the field in green if it's valid
-  // circle the field in red if it's not valid
-  // display the error message below the field
+// ADD ERROR MESSAGES
+// circle the field in green if it's valid
+// circle the field in red if it's not valid
+// display the error message below the field
 
   if (validateFirstName (first) == true) {
     numberValidFields++;
@@ -237,20 +225,13 @@ function validate() {
   if (validateCheckbox1 (checkbox1) == true) {
     numberValidFields++;
   }
-
-    
-  console.log('Fonction validate!');
-  const numberOfFields = formData.length;                                 // number of formData elements
-  console.log('numberOfFields :' + numberOfFields);  
    
-  console.log ('numberValidFields:' + numberValidFields);
-
+  const numberOfFields = formData.length;                                 // number of formData elements
+  
   if (numberValidFields == 6) {
-     console.log ('Form is valid');
     return true;
   }
   else {
-    console.log ('Form is not valid');
     return false;
   }
 }
@@ -260,14 +241,14 @@ function validate() {
 form.addEventListener('submit', submitForm);
 
 function submitForm(element) {
-    element.preventDefault();
-    validate();
+  element.preventDefault();
+  validate();
 
-    if (document.querySelectorAll('[data-error-visible=true]').length == 0) {
-        modal.style.display = 'none';
-        form.reset();
-        openThanksModal();
-    }
+  if (document.querySelectorAll('[data-error-visible=true]').length == 0) {
+      modal.style.display = 'none';
+      form.reset();
+      openThanksModal();
+  }
 }
 
 
@@ -282,18 +263,13 @@ function checkName (name) {
     return;
   }
 
-  console.log ('Function check of name');
-  console.log ('Name value :' + name);
-
   let regexName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
 
 
   if (name.match(regexName) && name != "" && name.length >= 2) {
-    console.log ('Name is correct');
     return true;
   }
   else {
-    console.log ('Name is incorrect');
     return false;
   }
 }
@@ -303,19 +279,14 @@ function checkName (name) {
 // return true if email is correct
 function checkEmail (email) {
 
-  console.log ('Function check of email');
-  console.log ('Email value :' + email);
-
   let regexEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]{2,}[.][a-zA-Z]{2,3}$/;  
 
-  if (email.value.match(regexEmail))      {
-    console.log ('Valid email address');
+  if (email.value.match(regexEmail))  {
     return true;
   }          
-	else {
-		console.log ('Invalid email address');
+    else {
     return false;
-	}
+}
 }
 
 // ------------------------------------------------------------------------------ BIRTHDATE
@@ -329,8 +300,7 @@ function checkBirthdate (birthdate) {
   } 
   else {   
     showError(birthdate);     
-    birthdate.parentElement.setAttribute("data-error", "Veuillez saisir une date de naissance valide");                
-    console.log ('Show error message');
+    birthdate.parentElement.setAttribute("data-error", "Veuillez saisir une date de naissance valide");    
     return;
   }  
   if (isPastDate(birthdate.value) == true) {
@@ -338,8 +308,7 @@ function checkBirthdate (birthdate) {
   }
   else {   
     showError(birthdate);     
-    birthdate.parentElement.setAttribute("data-error", "Veuillez saisir une date de naissance inferieur à la date du jour");                 
-    console.log ('Show error message');
+    birthdate.parentElement.setAttribute("data-error", "Veuillez saisir une date de naissance inferieur à la date du jour");  
     return;
   }
   if (controlAge(birthdate.value) == true) {
@@ -348,7 +317,6 @@ function checkBirthdate (birthdate) {
     else {   
       showError(birthdate);        
       birthdate.parentElement.setAttribute("data-error", "Cette espace est interdite aux personnes mineurs");              
-      console.log ('Show error message');
       return true;
     }  
 }
@@ -359,12 +327,9 @@ function checkBirthdate (birthdate) {
 function checkBirthdateFormat (birthdate) {
   let birthdateRegex = /^(19|20)\d{2}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
   if (birthdateRegex.test(birthdate)) {
-    console.log ('Birthdate format is correct');    
     return true;
   }
   else {
-    console.log ('Birthdate format is not correct');
-
     return false;
   }
 }
@@ -373,18 +338,14 @@ function checkBirthdateFormat (birthdate) {
 // parameter : birthdate is a date's value
 // return true if birthdate is in the past
 function isPastDate (birthdate) {  
-  console.log (birthdate);
   
   let birthday = new Date (birthdate);
   let today = new Date();
-  console.log (today > birthday);
-
+  
   if (today > birthday) {
-    console.log ('Birthdate is inferior of today');
     return true;
   }
   else {
-    console.log ('Birthdate is can not be superior of today');
     return false;
   } 
 }
@@ -393,21 +354,18 @@ function isPastDate (birthdate) {
 // parameter : birthdate is a date's value
 // return true if age is > 18 years old
 function controlAge (birthdate) {
-    const today = new Date ();
-    const actualYear = today.getFullYear ();
+  const today = new Date ();
+  const actualYear = today.getFullYear ();
 
-    console.log ('Actual year :' + actualYear);
-    const year = birthdate.substring(0,4);                             // Year (The first 4 characters of the string starting from 0)
+  const year = birthdate.substring(0,4);                             // Year (The first 4 characters of the string starting from 0)
 
-    const actualAge = actualYear - year;
-    if (actualAge > 18) {
-      console.log ('Age is valid');      
-      return true;
-    }
-    else {
-      console.log ('Age is not valid');
-      return false;
-    }
+  const actualAge = actualYear - year;
+  if (actualAge > 18) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 
@@ -417,15 +375,11 @@ function controlAge (birthdate) {
 // return true if a value is a number
 function checkQuantity (quantity) {
 
-  console.log ('Function check of contests');
-  console.log ('Contests value :' + (quantity));
 
   if (isNaN(quantity) || (quantity.value) == "")  {
-    console.log ('Quantity is not a number');
     return false;
   }
   else if (quantity >= 0 && quantity < 100) {
-    console.log ('Quantity is a number');
     return true;
   } 
 }
@@ -438,19 +392,17 @@ function checkQuantity (quantity) {
 // return false if none are checked, or there are no radio buttons
 function checkLocation (radioLocation) {
 
-	if (!radioLocation) {
-		return false; }
+  if (!radioLocation) {
+  return false; }
 
-	const radioLength = radioLocation.length;
-  console.log ('Radio length:' + radioLength);
-
-	for (let i = 0; i < radioLength; i++) {
-		if (radioLocation[i].checked) {
-      console.log ('City :' + radioLocation[i].value);
-			return true;
-		}
-	}
-	return false;
+const radioLength = radioLocation.length;
+  
+for (let i = 0; i < radioLength; i++) {
+  if (radioLocation[i].checked) {
+    return true;
+  }
+}
+return false;
 }
 
 
@@ -460,7 +412,7 @@ function checkLocation (radioLocation) {
 // return true if checkbox1 is checked
 // return false if not checked or there is not checked
 function checkCheckbox () {
-  
+
   if (document.reserve.conditions.checked) {
     document.getElementById("checkbox1").innerHTML = "";
     return true;
@@ -471,8 +423,10 @@ function checkCheckbox () {
   }
 }
 
+
 // =================================================================================================
 // =================================================================================================
+
 
 //TO DO LIST #4 : MODAL THANKS
 
@@ -498,6 +452,9 @@ closeThanksBtn.addEventListener('click', closeThanksModal);
 // =================================================================================================
 // =================================================================================================
 
-//Add a responsive HAMBURGER Menu 
+// Add a responsive HAMBURGER Menu 
 burgerBtn.addEventListener('click', editNav);
+
+
+
 
